4d7a53ccea11c2b389af662e91a98ac2c23c612c
Jan Kotrlik <janko@salsitasoft.com>
2022-09-10 00:41:54 +0200

    No exec for now

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 touch /var/log/app.log
-exec 2>&1 >( tee -a /var/log/app.log )
+#exec 2>&1 >( tee -a /var/log/app.log )
 
 echo "Setting up logging."
 if [ ! -z ${PAPERTRAIL_BASE64+x} ] ; then
