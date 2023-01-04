c250a69e955ca3a7c72beda8bd8018c38acb8e73
Jan Kotrlik <janko@salsitasoft.com>
2023-01-04 01:03:03 +0100

    Fix tarball name

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ eocre
 echo "Getting build from S3."
 aws --profile herokles s3 cp s3://${HEROKLES_AWS_S3_BUILDS_BUCKET}/${S3_FOLDER_NAME}/product.tgz . >/dev/null
 
-echo "Unpacking product.zip."
+echo "Unpacking product.tgz."
 tar xzf product.tgz
 rm -rf product.tgz
 
